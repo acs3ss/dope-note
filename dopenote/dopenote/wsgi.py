96a3fe84c368d339ed14aec53fc04baedb9ce445
Austin Sullivan <acs3ss@virginia.edu>
2018-09-22 14:16:18 -0400

    getting started

---
+++ dopenote/dopenote/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dopenote project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dopenote.settings')
+
+application = get_wsgi_application()
