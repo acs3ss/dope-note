97b0c4adaf067a523836eb2e2073c80c5eb9266d
Winston Liu <50Wliu@users.noreply.github.com>
2018-09-22 18:27:17 -0400

    Make subtitle downloading more versatile

--- scripts/subtitle-downloader.py
+++ scripts/subtitle-downloader.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python3
 
+import json
 import re
 import urllib.parse, urllib.request
 
 def download_subtitles(video):
     id = retrieve_id(video)
-    url = "https://www.youtube.com/api/timedtext?lang=en&fmt=vtt&name=&v={0}".format(id)
-    urllib.request.urlretrieve(url, id)
+
+    # Lots of the following taken from you-get
+    req = urllib.request.Request(video)
+
+    response = urllib.request.urlopen(req)
+    data = response.read()
+
+    # Handle HTTP compression for gzip and deflate (zlib)
+    content_encoding = response.getheader('Content-Encoding')
+    if content_encoding == 'gzip':
+        data = ungzip(data)
+    elif content_encoding == 'deflate':
+        data = undeflate(data)
+
+    # Decode the response body
+    match = re.search(r'charset=([\w-]+)', response.getheader('Content-Type'))
+    if match:
+        data = data.decode(match.group(1))
+    else:
+        data = data.decode('utf-8', 'ignore')
+
+    video_page = data
+    ytplayer_config = json.loads(re.search('ytplayer.config\s*=\s*([^\n]+?});', video_page).group(1))
+    try:
+        caption_tracks = json.loads(ytplayer_config['args']['player_response'])['captions']['playerCaptionsTracklistRenderer']['captionTracks']
+        for ct in caption_tracks:
+            urllib.request.urlretrieve(ct['baseUrl'], id + '_' + ct['languageCode'])
+    except:
+        pass
 
 def retrieve_id(url):
     # Adopted from you-get
