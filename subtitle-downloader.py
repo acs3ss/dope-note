14af4da287162da8410e85c92219c9126ea0862d
Winston Liu <50Wliu@users.noreply.github.com>
2018-09-22 17:03:46 -0400

    Basic subtitle downloader script for Youtube

---
+++ subtitle-downloader.py
@@ -0,0 +1,21 @@
+import re
+import urllib.parse, urllib.request
+
+def download_subtitles(video):
+    id = retrieve_id(video)
+    url = "https://www.youtube.com/api/timedtext?lang=en&fmt=vtt&name=&v={0}".format(id)
+    urllib.request.urlretrieve(url, id)
+
+def retrieve_id(url):
+    # Adopted from you-get
+    match = re.search(r'(?:youtube\.com/(?:embed|v|watch)|youtu\.be)/([^/?]+)', url)
+    if match:
+        return match.group(1)
+    else:
+        try:
+            return urllib.parse.parse_qs(urllib.parse.urlparse(url).query)['v'][0]
+        except:
+            return None
+
+
+download_subtitles("https://www.youtube.com/watch?v=jNQXAC9IVRw")
