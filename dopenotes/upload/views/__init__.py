099dd454c81352a8c9ced46b1c776c4dd4a40da4
alewis98 <anjale98@gmail.com>
2018-09-22 17:29:15 -0400

    Fixed file structure

---
+++ dopenotes/upload/views/__init__.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm, PasswordResetForm
+from django.contrib.auth.models import User
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
+from . import account, referee
+
+def home(request):
+    return render(request, 'upload.html')
