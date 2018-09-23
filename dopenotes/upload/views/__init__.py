3e84c94840077f5ac7756a5354f0bef8cc1b2e40
Winston Liu <50Wliu@users.noreply.github.com>
2018-09-22 22:19:11 -0400

    Start detail pages

--- dopenotes/upload/views/__init__.py
+++ dopenotes/upload/views/__init__.py
@@ -1,19 +1,27 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm, PasswordResetForm
 from django.contrib.auth.models import User
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
+from django.urls import reverse
+
 from upload.forms import *
 
 
 def home(request):
     if request.method == 'POST':
         form = UploadVideoForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
-        return HttpResponse("Good job!")
+        return HttpResponseRedirect(reverse('upload:detail', args=(form.cleaned_data['url'],)))
     else:
         form = UploadVideoForm()
         args = {'form': form}
         return render(request, 'upload_form.html', args)
+
+def video_detail_view(request, url=None):
+    obj = get_object_or_404(Video, url=url)
+    args = {'url': obj}
+    return render(request, 'detail_view.html', args)
